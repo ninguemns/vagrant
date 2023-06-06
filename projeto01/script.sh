7fffa77731c031177f6a2d2ffec60e56f8e5b11c
ninguemns <netonetworks@outlook.com>
2023-06-06 19:02:36 -0300

    vagrant_projeto01_instala_app

---
+++ projeto01/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt update
+sudo apt install -y vim
+sudo apt install -y curl
+sudo apt install -y telnet
+sudo apt install -y unzip
+sudo apt install -y wget
+sudo apt install -y net-tools
+sudo apt install -y htop
+sudo apt install -y nmap
+sudo useradd joaquim
